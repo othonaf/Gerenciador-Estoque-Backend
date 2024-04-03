9510e3d91b8f2d9c2a2cc71643f161930f4879c4
othonaf <othon.ab@hotmail.com>
2024-04-03 14:02:39 -0300

    Inserindo arquivo vercel.json

--- tables.sql
+++ tables.sql
@@ -1,3 +1,4 @@
+-- Active: 1712061061117@@dpg-co5k1ve3e1ms73b7jn8g-a.ohio-postgres.render.com@5432@gerenciador_de_estoque
 
 create type perfilTipo as ENUM ('admin', 'normal', 'mestre');
 
@@ -12,22 +13,23 @@ CREATE TABLE if NOT exists usuario (
 );
 
 create table if not exists produto (
-    codProd NUMERIC not null default primary key,
+    codProd NUMERIC not null primary key,
     descricao text not null unique,
     valorDeCompra DECIMAL not NULL,
     valorDeVenda DECIMAL NOT NULL,
     created_at timestamp with time zone not null default now(),
     vencimento timestamp with time zone not null,
-    quantidade NUMERIC NOT NULL
-
+    quantidade NUMERIC NOT NULL,
+    setor VARCHAR(20)
 );
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
 CREATE Table if not exists vendas (
     id UUID NOT NULL default uuid_generate_v4() primary key,
     data TIMESTAMP with time zone not null DEFAULT now(),
     valorTotal decimal NOT null,
-    vendedor NUMERIC REFERENCES usuario (login),
-    
-
+    vendedor NUMERIC REFERENCES usuario (cpf)
 );
 
 CREATE TABLE IF NOT EXISTS venda_produto (
@@ -36,3 +38,9 @@ CREATE TABLE IF NOT EXISTS venda_produto (
     PRIMARY KEY (venda_id, produto_id),
     total_lucro numeric NOT NULL
 );
+
+SELECT * FROM usuario;
+
+SELECT * FROM vendas;
+
+SELECT * FROM venda_produto;
